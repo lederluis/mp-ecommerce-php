21da9a2b29cebc43eedba162ceca63c1977001fd
Luis Leder <leaderluis20@gmail.com>
2020-08-21 03:31:15 -0300

    correccion en credencial

--- mpago.php
+++ mpago.php
@@ -7,7 +7,7 @@ $url_principal = isset($_SERVER['SERVER_NAME']) ? $_SERVER['SERVER_NAME'] : null
 $url_principal = "https://".$url_principal;
 
 // Agrega credenciales
-MercadoPago\SDK::setAccessToken("APP_USR-6317427424180639-042414-47e969706991d3a442922b0702a0da44-469485398 "); 
+MercadoPago\SDK::setAccessToken("APP_USR-6317427424180639-042414-47e969706991d3a442922b0702a0da44-469485398"); 
 MercadoPago\SDK::setIntegratorId("dev_24c65fb163bf11ea96500242ac130004");
 
 // Crea un objeto de preferencia
@@ -71,4 +71,4 @@ $preference->payment_methods = array(
 $preference->save();
 
 ?>
-<a  class="mercadopago-button" href="<?php echo $preference->init_point; ?>">Pagar la compra</a>
+<a  class="mercadopago-button" style="padding:5px 10px;" href="<?php echo $preference->init_point; ?>">Pagar la compra</a>
