8b7605d50cded319a7689bbc87620e1e8b2584cc
Luis Leder <leaderluis20@gmail.com>
2020-08-23 03:40:47 -0300

    pasar parametro a get

--- notification.php
+++ notification.php
@@ -1,6 +1,6 @@
 <?php
 $key = "APP_USR-6317427424180639-042414-47e969706991d3a442922b0702a0da44-469485398";
-$payment = $_POST["id"];
+$payment = $_GET["id"];
 
 $json = file_get_contents("https://api.mercadopago.com/v1/payments/$payment?access_token=$key");
 $file = __DIR__ . "/responses/test.json";
