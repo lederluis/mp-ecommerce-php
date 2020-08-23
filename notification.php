7855d29b5c3e66122afc6b7d7f56edea05b06520
Luis Leder <leaderluis20@gmail.com>
2020-08-23 03:25:30 -0300

    verificando valor de archivo

--- notification.php
+++ notification.php
@@ -3,7 +3,7 @@ $key = "APP_USR-6317427424180639-042414-47e969706991d3a442922b0702a0da44-4694853
 $payment = $_POST["id"];
 
 $json = file_get_contents("https://api.mercadopago.com/v1/payments/$payment?access_token=$key");
-$file = __DIR__ . "/responses/$payment.json";
+$file = __DIR__ . "/responses/test.json";
 
 $fh = fopen($file, 'w');
 fwrite($fh, $json);
