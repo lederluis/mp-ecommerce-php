98058b20b521ab6254a9baf9a3d75d9bcd376003
Luis Leder <leaderluis20@gmail.com>
2020-08-23 04:13:21 -0300

    guardar entrada

--- notification.php
+++ notification.php
@@ -1,12 +1,15 @@
 <?php
-$key = "APP_USR-6317427424180639-042414-47e969706991d3a442922b0702a0da44-469485398";
-$payment = $_GET["id"];
+header("HTTP/1.1 200 OK");
 
-$json = file_get_contents("https://api.mercadopago.com/v1/payments/$payment?access_token=$key");
+// $key = "APP_USR-6317427424180639-042414-47e969706991d3a442922b0702a0da44-469485398";
+// $payment = $_GET["id"];
+
+// $json = file_get_contents("https://api.mercadopago.com/v1/payments/$payment?access_token=$key");
+$notifications = file_get_contents("php://input");
 $file = __DIR__ . "/responses/test.json";
 
 $fh = fopen($file, 'w');
-fwrite($fh, $json);
+fwrite($fh, $notifications);
 fclose($fh);
 
 ?>  
