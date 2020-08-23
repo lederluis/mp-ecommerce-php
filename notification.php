f4496c84132b75d02ebc49205cdbd2dd45b11012
Luis Leder <leaderluis20@gmail.com>
2020-08-23 02:49:17 -0300

    cambiando metodo de notificacion por error en documetnación mpago

--- notification.php
+++ notification.php
@@ -1,16 +1,12 @@
 <?php
-// SDK de Mercado Pago
-require __DIR__ .  '/vendor/autoload.php';
+$key = "APP_USR-6317427424180639-042414-47e969706991d3a442922b0702a0da44-469485398";
+$payment = $_POST["id"];
 
-MercadoPago\SDK::setAccessToken("APP_USR-6317427424180639-042414-47e969706991d3a442922b0702a0da44-469485398"); 
-$payment = MercadoPago\Payment.find_by_id($_POST["id"]);
-
-$request = $_POST;
-$jsonencoded = json_encode($request,JSON_UNESCAPED_UNICODE);
+$json = file_get_contents("https://api.mercadopago.com/v1/payments/$payment?access_token=$key");
 $file = __DIR__ . "/responses/$payment.json";
 
 $fh = fopen($file, 'w');
-fwrite($fh, $jsonencoded);
+fwrite($fh, $json);
 fclose($fh);
 
-?>
+?>  
